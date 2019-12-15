5a5fa03bbf0adba9a2505b89dbcf9c84a6d2c183
ThomasB123 <thomas@thebutterfields.org>
2019-12-15 20:22:18 +0000

    Update 003.c

--- 003.c
+++ 003.c
@@ -6,9 +6,30 @@ What is the largest prime factor of the number 600851475143 ?
 // Answer: 6857
 
 #include <stdio.h>
+#include <stdbool.h>
+long num = 600851475143;
+int x = 200000;
+bool factor;
 int main(){
-    int out = 0;
-
-    printf("%d\n", out);
+    while (x > 0)
+    {
+        if (num % x == 0)
+        {
+            factor = true;
+            for (int i = 2; i < x; i++)
+            {
+                if (x % i == 0)
+                {
+                    factor = false;
+                }
+            }
+            if (factor)
+            {
+                printf("%d\n", x);
+                break;
+            }
+        }
+        x -= 1;
+    }
     return 0;
 }
