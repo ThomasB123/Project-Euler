34a911fccd48df964b342588aefe1997cdb96137
ThomasB123 <thomas@thebutterfields.org>
2019-12-15 02:33:23 +0000

    Update 003.c

--- 003.c
+++ 003.c
@@ -4,3 +4,11 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 */
 // Answer: 6857
+
+#include <stdio.h>
+int main(){
+    int out = 0;
+
+    printf("%d\n", out);
+    return 0;
+}
